1fdd9f74847a445be4071753d6964030f37a72ff
Que Pham <quep@vnext.com.vn>
2018-11-14 17:07:35 +0700

    #4 LifeCycle Methods

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react'
 import { View, Text } from 'react-native'
 
 class AlbumList extends Component{
+    componentWillMount() {
+        console.log('ComponentWillMount in AlbumList');
+        let a = 2;
+        console.log(this);
+    }
+
     render() {
         return (
             <View>
