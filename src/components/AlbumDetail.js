3cd0740f5e03ce31f56245c0590e1ae77f183515
Que Pham <quep@vnext.com.vn>
2018-11-15 17:41:07 +0700

    #9 Display Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,21 +4,25 @@ import Card from "./Card";
 import CardSection from "./CardSection";
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image} = album
-    const {thumnailStyle, headerContentStyle} = styles
+    const {title, artist, thumbnail_image, image} = album
+    const {thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle} = styles
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image
-                        style={thumnailStyle}
+                        style={thumbnailStyle}
                         source={{uri: thumbnail_image}}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={imageStyle} source={{uri: image}} />
             </CardSection>
         </Card>
     )
@@ -30,10 +34,25 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
-    thumnailStyle: {
+    thumbnailStyle: {
         width: 50,
         height: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
+
 }
 
 export default AlbumDetail
