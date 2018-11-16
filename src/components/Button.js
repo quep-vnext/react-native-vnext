506786106505f26857f55d42a4c919c7f87a8e24
Que Pham <quep@vnext.com.vn>
2018-11-16 16:42:39 +0700

    #9 Handling User Input with Buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,23 @@
-import React from  'react'
-import {Text} from  'react-native'
+import React from 'react'
+import {Text, TouchableOpacity} from 'react-native'
 
-const Button = () =>  {
+const Button = () => {
     return (
-        <Text>
-            Click Me Up!!!
-        </Text>
+        <TouchableOpacity style={styles.clickButtonStyle}>
+            <Text>
+                Click Me Up!!!!!
+            </Text>
+        </TouchableOpacity>
     )
 }
 
+const styles = {
+    clickButtonStyle: {
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+    }
+}
+
 export default Button
+
