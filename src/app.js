babc5f5d210c1c7159d13654c959792345117149
Adnet <adnetplus.dev@gmail.com>
2018-11-18 01:00:46 +0700

    #13 Firebase Client SetUp

--- src/app.js
+++ src/app.js
@@ -2,7 +2,20 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {Header} from './components'
 
+import firebase from 'firebase'
+
 class App extends Component {
+
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4",
+			authDomain: "v-gadern.firebaseapp.com",
+			databaseURL: "https://v-gadern.firebaseio.com",
+			projectId: "v-gadern",
+			storageBucket: "v-gadern.appspot.com",
+			messagingSenderId: "1027744237998"
+		})
+	}
 	render() {
 		return (
 			<View>
