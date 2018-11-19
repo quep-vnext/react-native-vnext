3d7f0add52fb53dc797315381117f74c9b3b1494
Adnet <adnetplus.dev@gmail.com>
2018-11-19 10:47:06 +0700

    #13 Firebase Client SetUp

---
+++ src/component/FirebaseConfig.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase'
+
+var config = {
+	apiKey: "AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4",
+	authDomain: "v-gadern.firebaseapp.com",
+	databaseURL: "https://v-gadern.firebaseio.com",
+	projectId: "v-gadern",
+	storageBucket: "v-gadern.appspot.com",
+	messagingSenderId: "1027744237998"
+};
+export  const firebaseApp = firebase.initializeApp()
