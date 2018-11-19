cb5bdd071f012b8cd35cf0e3525888d3a5add6cf
Que Pham <quep@vnext.com.vn>
2018-11-19 13:14:42 +0700

    #9 React Native Grid UI

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # react-native-vnext
 
 Here is deploy product branch.
-
-
-Master Branch
-Master Branch 1
-
-Development Branch
-Development Branch 1
-
-Development Junior
-Development Junior 1
-
-Development Senior
-Development Senior 1
-
-Kaihatsu Branch
-Kaihatsu Branch 1
-
--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
 import {Header} from './components'
 import {LoginForm} from './component/LoginForm'
 
 class App extends Component {
 
-	componentWillMount() {
-		firebase.initializeApp({
-			apiKey: "AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4",
-			authDomain: "v-gadern.firebaseapp.com",
-			databaseURL: "https://v-gadern.firebaseio.com",
-			projectId: "v-gadern",
-			storageBucket: "v-gadern.appspot.com",
-			messagingSenderId: "1027744237998"
-		})
-	}
+	// componentWillMount() {
+	// 	firebase.initializeApp({
+	// 		apiKey: "AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4",
+	// 		authDomain: "v-gadern.firebaseapp.com",
+	// 		databaseURL: "https://v-gadern.firebaseio.com",
+	// 		projectId: "v-gadern",
+	// 		storageBucket: "v-gadern.appspot.com",
+	// 		messagingSenderId: "1027744237998"
+	// 	})
+	// }
 
 	render() {
 		return (
