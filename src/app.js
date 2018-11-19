eb5805362ad183af67e26398b662db263cbae544
Que Pham <quep@vnext.com.vn>
2018-11-19 14:57:20 +0700

    Handle Input Login Form

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
-// import * as firebase from 'firebase'
+import * as firebase from 'firebase'
 import {Header} from './components'
 import {LoginForm} from './component/LoginForm'
 
 class App extends Component {
 
-	// componentWillMount() {
-	// 	firebase.initializeApp({
-	// 		apiKey: "AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4",
-	// 		authDomain: "v-gadern.firebaseapp.com",
-	// 		databaseURL: "https://v-gadern.firebaseio.com",
-	// 		projectId: "v-gadern",
-	// 		storageBucket: "v-gadern.appspot.com",
-	// 		messagingSenderId: "1027744237998"
-	// 	})
-	// }
+	componentWillMount() {
+		console.log('Config Firebase');
+		firebase.initializeApp({
+			apiKey: 'AIzaSyD0qrO9sx5fJOQpoT0kYG7vLNHgXnh9Ik4',
+			authDomain: 'v-gadern.firebaseapp.com',
+			databaseURL: 'https://v-gadern.firebaseio.com',
+			projectId: 'v-gadern',
+			storageBucket: 'v-gadern.appspot.com',
+			messagingSenderId: '1027744237998'
+		})
+	}
 
 	render() {
 		return (
 			<View>
-				<Header textHeader="Authentications" />
+				<Header textHeader='Authentications' />
 				<LoginForm />
 			</View>
 		)
--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
-import {View, Button, Text} from 'react-native'
+import {View, Button, Text, TextInput} from 'react-native'
 import {ButtonClick, Card, CardSection} from '../components'
 
 class LoginForm extends Component {
 	render() {
+		console.log('Login Form');
 		return (
 			<Card>
 				<CardSection>
-
+					<TextInput style={{height: 20, width: 100}} />
 				</CardSection>
 
 				<CardSection>
