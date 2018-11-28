29e8fc018ef969680bfcb99779656299177f8966
Que Pham <quep@vnext.com.vn>
2018-11-28 14:51:08 +0700

    Navigation Between Screen #2 (Using push)

--- src/Screen/Detail.js
+++ src/Screen/Detail.js
@@ -1,12 +1,17 @@
 
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Button} from 'react-native'
 
 export default class Detail extends Component{
     render() {
         return(
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <Text>Detail Screen</Text>
+
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('DetailScreen')}
+                />
             </View>
         )
     }
