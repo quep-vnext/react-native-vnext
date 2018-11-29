dc0ee09331b6f9d36dca508971bc3f5513996097
Que Pham <quep@vnext.com.vn>
2018-11-29 18:46:49 +0700

    Icon Apps

--- src/component/DeleteIcon.js
+++ src/component/DeleteIcon.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {View, Text, Image, Platform} from 'react-native'
-// import Icon from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const DeleteIcon = () => {
     return (
         <View>
-            <Text>Hello world</Text>
+            <Icon
+                name={Platform.OS === "ios" ? "ios-add" : "md-add"}
+                color="#ccc"
+                size={25}
+            />
         </View>
     )
 }
